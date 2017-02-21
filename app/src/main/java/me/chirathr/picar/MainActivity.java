2e256ea2ef481a03c65fc8c4c3139893bf248093
Chirath02 <chirath.02@gmail.com>
2017-02-21 21:19:18 +0530

    added button

--- app/src/main/java/me/chirathr/picar/MainActivity.java
+++ app/src/main/java/me/chirathr/picar/MainActivity.java
@@ -2,6 +2,9 @@ package me.chirathr.picar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,61 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        Button upButton = (Button) findViewById(R.id.up);
+
+        upButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    // Pressed
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    // Released
+                }
+                return true;
+            }
+        });
+
+        Button leftButton = (Button) findViewById(R.id.left);
+
+        leftButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    // Pressed
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    // Released
+                }
+                return true;
+            }
+        });
+
+        Button rightButton = (Button) findViewById(R.id.right);
+
+        rightButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    // Pressed
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    // Released
+                }
+                return true;
+            }
+        });
+
+
+        Button downButton = (Button) findViewById(R.id.down);
+
+        downButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    // Pressed
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    // Released
+                }
+                return true;
+            }
+        });
     }
 }
