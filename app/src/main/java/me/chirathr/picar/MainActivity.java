98107ab399843a288f3847ac93b31f68df2eb9a7
Chirath02 <chirath.02@gmail.com>
2017-03-03 23:53:19 +0530

    app sents data

--- app/src/main/java/me/chirathr/picar/MainActivity.java
+++ app/src/main/java/me/chirathr/picar/MainActivity.java
@@ -8,12 +8,23 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class MainActivity extends AppCompatActivity {
 
-    Server server;
     TextView infoip, msg;
+    OutputStream outputStream;
+    static final int socketServerPORT = 8080;
+    static final String socketServerIP = "192.168.43.1";
+    final BlockingQueue<String> queue = new LinkedBlockingQueue<String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +33,70 @@ public class MainActivity extends AppCompatActivity {
 
         infoip = (TextView) findViewById(R.id.infoip);
         msg = (TextView) findViewById(R.id.msg);
-        server = new Server(this);
-//        server.connect();
-//        infoip.setText(server.getIpAddress() + ":" + server.getPort());
+
+
+        Thread running = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Log.e("Done", "Done");
+
+                try {
+                    // create ServerSocket using specified port
+
+                    ServerSocket serverSocket = new ServerSocket(socketServerPORT);
+
+                    // block the call until connection is created and return Socket object
+
+                    Socket socket = serverSocket.accept();
+
+                    Log.e("Done", "Done");
+
+
+                    outputStream = socket.getOutputStream();
+
+                    PrintStream printStream = new PrintStream(outputStream);
+
+                    while (true) {
+                        try {
+                            String data = queue.take();
+                            printStream.print(data);
+                            Log.e("Done", data);
+                            //handle the data
+                            if(data.equals("Exit")) {
+                                break;
+                            }
+                        } catch (InterruptedException e) {
+                            System.err.println("Error occurred:" + e);
+                        }
+                    }
+
+                    printStream.close();
+
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        running.start();
+
 
         Button upButton = (Button) findViewById(R.id.up);
 
         upButton.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
+                String text = "";
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
                     Context context = getApplicationContext();
-                    String text = "a.key-down";
+                    text = "a.key-down";
+                    queue.offer(text);
 
-                    //server.setMsg(text);
                 } else if (event.getAction() == MotionEvent.ACTION_UP) {
                     Context context = getApplicationContext();
-                    String text = "a.key-up";
-
-//                    server.setMsg(text);
+                    text = "a.key-up";
+                    queue.offer(text);
                 }
                 return true;
             }
@@ -88,14 +144,11 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-
-
-
     }
 
     @Override
     protected void onDestroy() {
+        queue.offer("Exit");
         super.onDestroy();
-        server.onDestroy();
     }
 }
